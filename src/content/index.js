47f760fe1ea70e2ea94eceadb756522d336f9f31
lucas gonçalves <lucas.goncalves@foster.com.br>
2020-10-17 13:56:08 -0300

    finished project

--- src/content/index.js
+++ src/content/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Logo from '../image/logo.png';
-import '../content/style.scss';
+import '../content/style.css';
 import api from '../services/matchboxbrasil.json';
 
 class Ranking extends Component {
@@ -9,22 +9,53 @@ class Ranking extends Component {
         ranking: [],
     }
 
-    async componentDidMount() {
-        const response = await api;
-        this.setState({ ranking: response.data });
+    componentDidMount() {
+        const response = api;
+
+        const total = response.data.reduce((total, current) => {
+            if (current.positive) {
+                return total + current.positive;
+            }
+            return total;
+        }, 0);
+
+        const ranking = response.data
+            
+            .map((item) => {
+                if (item.positive) {
+                    item.positive_percent = Math.ceil((item.positive * 100) / total);
+                } else {
+                    item.positive_percent = 0;
+                }
+
+                if (item.negative) {
+                    item.negative_percent = Math.ceil((item.negative * 100) / total);
+                } else {
+                    item.negative_percent = 0;
+                }
+
+                return item;
+            });
+            
+            ranking.sort(function(a, b){
+                if(a.positive < b.positive){
+                    return 1
+                }
+                if(a.positive > b.positive){
+                    return -1
+                }
+                else{
+                    return 0
+                }
+            })
+                
+
+        this.setState({ ranking });
     }
-    
+
 
     render() {
         const { ranking } = this.state;
-        function sortfunction(a, b){
-            return (a - b) //faz com que o array seja ordenado numericamente e de ordem crescente.
-          }
-          ranking.map((rank) =>{
-              
-              console.log(rank.positive);
-            })
-          
         return (
             <div className="container">
                 <div className="content">
@@ -32,7 +63,7 @@ class Ranking extends Component {
                     <div className="ranking">
                         <h1>ranking</h1>
                     </div>
-                    {ranking.map((item, i = 1) => (
+                    {ranking.map((item, i) => (
                         <div className="podios-ranking" key={item.__id}>
                             <div className="box-image">
                                 <img src={item.picture} alt={item.name} />
@@ -46,11 +77,11 @@ class Ranking extends Component {
                                 <div className="seta"></div>
                                 <div className="positive">
                                     <h5>Gostam</h5>
-                                    <div className="porcent">{ item.positive / 100 }%</div>
+                                    <div className="porcent">{item.positive_percent}%</div>
                                 </div>
                                 <div className="negative">
                                     <h5>Não Gostam</h5>
-                                    <div className="porcent">{ item.negative / 100 }%</div>
+                                    <div className="porcent">{item.negative_percent}%</div>
                                 </div>
                             </div>
                         </div>
